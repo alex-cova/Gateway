ab0f68e7017a203b5bfb37e849cfae8316f95ed5
Alejandro Covarrubias <alex@sicar.mx>
2021-10-10 18:57:30 -0500

    Async client request

--- Sources/App/EurekaJob.swift
+++ Sources/App/EurekaJob.swift
@@ -12,60 +12,66 @@ import Queues
 
 
 struct EurekaJob : ScheduledJob {
+    
     func run(context: QueueContext) -> EventLoopFuture<Void> {
         
         let eureka = Environment.get("eureka") ?? "localhost:8761"
         
-        do{
+        
             
             print("updating...")
+        
             var headers = HTTPHeaders()
-            headers.contentType = .json
+        headers.contentType = .json
+        headers.basicAuthorization = .init(username: "sQ9aSwUtM6RQFFRh", password: "NdKYx6fI+FFApmkKK3d8jGV0G49nYbHn")
+        
             
             let request = context.application.client
                 .get("http://\(eureka)/eureka/apps", headers: headers)
             
-            do {
             
-            let response = try request.wait()
             
-        
-                if(response.status == .ok){
-                    
-                    map.removeAll()
-                    
-                    if let body = response.body{
-                        do {
-                        let applications = try JSONDecoder().decode(Applications.self, from: body)
+            request.whenSuccess{ response in
+                
+                print("response: \(response.status)")
+                
+                        if(response.status == .ok){
                             
-                            for  app in applications.application {
+                            map.removeAll()
+                            
+                            if let body = response.body{
                                 
-                                var ip : [String] = []
-                                
-                                for ins in app.instance {
-                                    let route = "\(ins.ipAddr):\(ins.port)"
-                                    print("adding: \(route)")
-                                    ip.append(route)
+                                do {
+                                let applications = try JSONDecoder().decode(Applications.self, from: body)
+                                    
+                                    for  app in applications.application {
+                                        
+                                        var ip : [String] = []
+                                        
+                                        for ins in app.instance {
+                                            let route = "\(ins.ipAddr):\(ins.port)"
+                                            print("adding: \(route)")
+                                            ip.append(route)
+                                        }
+                                        
+                                        map[app.name] = ip
+                                    }
+                                    
+                                }catch {
+                                    print(error)
                                 }
-                                
-                                map[app.name] = ip
                             }
-                            
-                        }catch {
-                            print(error)
                         }
-                    }
-                }else{
-                    print("response: \(response.status)")
-                }
+                    
+            }
             
-        }catch{
-            print(error)
-        }
+            
+    
         
-        return context.eventLoop.makeSucceededFuture(())
+        return request
+            .transform(to: context.eventLoop.makeSucceededVoidFuture())
     }
-    }
+    
 }
     
     
