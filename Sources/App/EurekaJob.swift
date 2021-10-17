5a1db3c95f324e43467f92a4cb59ceda21eb4b0e
Alejandro Covarrubias <alex@sicar.mx>
2021-10-17 13:55:23 -0500

    envs

--- Sources/App/EurekaJob.swift
+++ Sources/App/EurekaJob.swift
@@ -11,67 +11,74 @@ import Vapor
 import Queues
 
 
-struct EurekaJob : ScheduledJob {
-    
+struct EurekaJob: ScheduledJob {
+
     func run(context: QueueContext) -> EventLoopFuture<Void> {
-        
+
         let eureka = Environment.get("eureka") ?? "localhost:8761"
-        
-        
-            
-            print("updating...")
-        
-            var headers = HTTPHeaders()
-        headers.contentType = .json
-        headers.basicAuthorization = .init(username: "sQ9aSwUtM6RQFFRh", password: "NdKYx6fI+FFApmkKK3d8jGV0G49nYbHn")
-        
-            
-            let request = context.application.client
-                .get("http://\(eureka)/eureka/apps", headers: headers)
-            
-            
-            
-            request.whenSuccess{ response in
-                
-                print("response: \(response.status)")
-                
-                        if(response.status == .ok){
-                            
-                            map.removeAll()
-                            
-                            if let body = response.body{
-                                
-                                do {
-                                let applications = try JSONDecoder().decode(Applications.self, from: body)
-                                    
-                                    for  app in applications.application {
-                                        
-                                        var ip : [String] = []
-                                        
-                                        for ins in app.instance {
-                                            let route = "\(ins.ipAddr):\(ins.port)"
-                                            print("adding: \(route)")
-                                            ip.append(route)
-                                        }
-                                        
-                                        map[app.name] = ip
-                                    }
-                                    
-                                }catch {
-                                    print(error)
-                                }
+
+        print("updating...")
+
+        var headers: HTTPHeaders?
+
+        let eurekaUser = Environment.get("EUREKA_USER")
+        let eurekaPass = Environment.get("EUREKA_PASS")
+
+        if eurekaUser != nil && eurekaPass != nil {
+
+            headers = HTTPHeaders()
+
+            headers?.contentType = .json
+            headers?.basicAuthorization = .init(username: eurekaUser!, password: eurekaPass!)
+        }
+
+        let url = "http://\(eureka)/eureka/apps"
+
+        let request = headers != nil ?
+
+                context.application.client
+                        .get("\(url)", headers: headers!) :
+                context.application.client.get("\(url)")
+
+
+        request.whenSuccess { response in
+
+            print("response: \(response.status)")
+
+            if (response.status == .ok) {
+
+                map.removeAll()
+
+                if let body = response.body {
+
+                    do {
+                        let applications = try JSONDecoder().decode(Applications.self, from: body)
+
+                        for app in applications.application {
+
+                            var ip: [String] = []
+
+                            for ins in app.instance {
+                                let route = "\(ins.ipAddr):\(ins.port)"
+                                print("adding: \(route)")
+                                ip.append(route)
                             }
+
+                            map[app.name] = ip
                         }
-                    
+
+                    } catch {
+                        print(error)
+                    }
+                }
             }
-            
-            
-    
-        
+
+        }
+
         return request
-            .transform(to: context.eventLoop.makeSucceededVoidFuture())
+                .transform(to: context.eventLoop.makeSucceededVoidFuture())
     }
-    
+
 }
     
     
